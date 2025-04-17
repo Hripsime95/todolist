7f10a39fe278d17027deb374c4cb87c960c00268
Hripsime Madoyan <h.vardoeva.95@gmail.com>
2025-04-17 16:15:57 +0400

    Empty project

--- src/App.tsx
+++ src/App.tsx
@@ -1,59 +1,7 @@
-import './App.css'
-import { TodoList } from './components/TodoList';
-import { TTaskFilter, TTask } from './components/types/type.ts';
-import { GenerateId } from './common/generateId.tsx';
-import { useState } from 'react';
-
 function App() {
-
-  let [tasks, setState] = useState<TTask[]>([
-    {
-      id: GenerateId(),
-      title: 'CSS',
-      type: 'checkbox',
-      isDone: true
-    },
-    {
-      id: GenerateId(),
-      title: 'JS',
-      type: 'checkbox',
-      isDone: true
-    },
-    {
-      id: GenerateId(),
-      title: 'React',
-      type: 'checkbox',
-      isDone: false
-    },
-    {
-      id: GenerateId(),
-      title: 'React Toolkit',
-      type: 'checkbox',
-      isDone: false
-    },
-  ]);
-
-  let [filter, setFilter] = useState<TTaskFilter>('all');
-  let tasksForTodoList = tasks;
-
-  if(filter === 'completed') tasksForTodoList = tasks.filter((t) => t.isDone === true);
-  if(filter === 'active') tasksForTodoList = tasks.filter((t) => t.isDone === false);
-
-  function filterTasks(filter: TTaskFilter){
-    setFilter(filter);
-  }
-
-
-  function removeTask(id: string) {
-    debugger;
-    let filteredTasks = tasks.filter( t => t.id !== id);
-    setState(filteredTasks);
-  }
-
+ 
   return (
-      <div className="app">
-        <TodoList title={'What to learn?'} tasks={tasksForTodoList} removeTask={removeTask} filterTasks={filterTasks}/>
-      </div>
+      <div></div>
   )
 }
 
--- src/common/generateId.tsx
+++
@@ -1,3 +0,0 @@
-export const GenerateId = () => {
-    return Date.now().toString(36) + Math.random().toString(36).substring(2);
-}
